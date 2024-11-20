69fb42a97c22c0dd3c4b14ccf0d6dfdee27563a6
Vinícius Andrade <vinicius_andrade2010@hotmail.com>
2024-11-20 19:24:45 -0300

    fix: .env

--- src/main/java/com/resftul/dscommerce/DscommerceApplication.java
+++ src/main/java/com/resftul/dscommerce/DscommerceApplication.java
@@ -1,11 +1,12 @@
 package com.resftul.dscommerce;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class DscommerceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DscommerceApplication.class, args);
+        run(DscommerceApplication.class, args);
     }
 }
