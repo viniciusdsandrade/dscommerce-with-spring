010f3d24be7d86f787ae9ba87f0286caa3e2b510
andrade <viniciusdsandrade0662@gmail.com>
2025-10-20 13:19:54 -0300

    feat: enable Spring Data web support with DTO serialization in application

--- src/main/java/com/resftul/dscommerce/DscommerceApplication.java
+++ src/main/java/com/resftul/dscommerce/DscommerceApplication.java
@@ -1,9 +1,12 @@
 package com.resftul.dscommerce;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import static org.springframework.boot.SpringApplication.run;
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @SpringBootApplication
 public class DscommerceApplication {
     public static void main(String[] args) {
