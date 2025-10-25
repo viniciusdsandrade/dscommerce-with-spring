3becbde27f7125958c128071eace0d410540d24c
andrade <viniciusdsandrade0662@gmail.com>
2025-10-25 15:23:16 -0300

    refactor: enhance readability in equals and hashCode methods of Category entity

--- src/main/java/com/resftul/dscommerce/entity/Category.java
+++ src/main/java/com/resftul/dscommerce/entity/Category.java
@@ -40,15 +40,26 @@ public class Category {
     public final boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null) return false;
-        Class<?> oEffectiveClass = obj instanceof HibernateProxy ? ((HibernateProxy) obj).getHibernateLazyInitializer().getPersistentClass() : obj.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+
+        Class<?> oEffectiveClass = obj instanceof HibernateProxy
+                ? ((HibernateProxy) obj).getHibernateLazyInitializer().getPersistentClass()
+                : obj.getClass();
+
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
+                : this.getClass();
+
         if (thisEffectiveClass != oEffectiveClass) return false;
+
         Category category = (Category) obj;
+
         return getId() != null && Objects.equals(getId(), category.getId());
     }
 
     @Override
     public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
     }
 }
